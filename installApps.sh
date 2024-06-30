827982223840d0fdc9838086dca5d4e3075af1fe
Ilija Makoveev <61517911+CrlnProgramming@users.noreply.github.com>
2024-07-01 00:35:54 +0300

    Update installApps.sh

--- installApps.sh
+++ installApps.sh
@@ -6,8 +6,10 @@ apt-get install -y \
     dotnet-sdk-7.0 \
     unzip \
     smartmontools \
+    net-tools \
     nmap \
     python3.8 \
+    golang-go \
     docker-ce docker-ce-cli containerd.io
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
