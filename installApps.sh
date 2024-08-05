23c87f3ab26fd1263ea75fc99ef5f929c2e8c1f0
Ilija Makoveev <61517911+CrlnProgramming@users.noreply.github.com>
2024-08-05 15:51:36 +0300

    added nuget

--- installApps.sh
+++ installApps.sh
@@ -13,7 +13,8 @@ apt-get install -y \
     nmap \
     python3.10 \
     python3-pip \
-    golang-go 
+    golang-go \
+    nuget
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
