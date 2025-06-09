b94c024b90004eb9c0be31a2203dcd86c0ce9a3d
Ilija Makoveev <61517911+CrlnProgramming@users.noreply.github.com>
2025-06-09 18:35:15 +0300

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y wget curl gnupg2 software-properties-common
+#apt-get install -f
+apt update && apt upgrade -y
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/keyrings/postgresql.gpg
-
-echo "deb [signed-by=/etc/apt/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-
-sudo apt update
-sudo apt install -y postgresql-15 postgresql-client-15
-
-export PATH="/usr/lib/postgresql/15/bin:$PATH"
-psql --version
-
-sudo ln -sf /lib/systemd/system/postgresql@15-main.service /etc/systemd/system/postgresql.service
-sudo systemctl daemon-reload
-
-sudo systemctl enable postgresql
-sudo systemctl start postgresql
-
-sudo apt-mark hold postgresql-15 postgresql-client-15
+apt-get install -y postgresql-client-15 postgresql-15
 
 echo "Проверка статуса PostgreSQL:"
-sudo systemctl status postgresql --no-pager
+systemctl status postgresql --no-pager
 
 echo "PostgreSQL 15 успешно установлен и работает!"
 echo "Подключиться к серверу: sudo -u postgres psql"
